a9e0057a094dcb4c9b2288921cd9881d482f1f02
User <User@192.168.1.21>
2026-06-04 17:30:44 -0300

    feat: inicializar projeto.

---
+++ conssecionaria-backend/src/main/java/com/conssecionaria/conssecionaria_backend/ConssecionariaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.conssecionaria.conssecionaria_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConssecionariaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConssecionariaBackendApplication.class, args);
+	}
+
+}
---
+++ conssecionaria-backend/src/test/java/com/conssecionaria/conssecionaria_backend/ConssecionariaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conssecionaria.conssecionaria_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConssecionariaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
