b0a82f937acbc300661a180ec53401d43066df70
User <User@192.168.1.21>
2026-06-08 13:31:17 -0300

    refactor: transformar o EmployeeResponseDTO em classe.

--- conssecionaria-backend/src/main/java/com/conssecionaria/conssecionaria_backend/model/dto/EmployeeResponseDTO.java
+++ conssecionaria-backend/src/main/java/com/conssecionaria/conssecionaria_backend/model/dto/EmployeeResponseDTO.java
@@ -5,17 +5,26 @@ import java.time.LocalDate;
 
 import com.conssecionaria.conssecionaria_backend.model.entity.enums.EmployeeStatus;
 import com.conssecionaria.conssecionaria_backend.model.entity.enums.EmployeeType;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
-public record EmployeeResponseDTO(
-		String id,
-		String name,
-		String matriculation,
-		String email,
-		String phone,
-		LocalDate birthDate,
-		String cpf,
-		BigDecimal salary,
-		Integer commission,
-		EmployeeStatus employeeStatus,
-		EmployeeType employeeType
-) {}
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeResponseDTO {
+	private String id;
+	private String name;
+	private String matriculation;
+	private String email;
+	private String phone;
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	private LocalDate birthDate;
+	private String cpf;
+	private BigDecimal salary;
+	private Integer commission;
+	private EmployeeStatus employeeStatus;
+	private EmployeeType employeeType;
+}
