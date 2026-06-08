cf6105dc0b0fb10ef6f73c3c6fdb3248a2456a50
User <User@192.168.1.21>
2026-06-08 16:32:43 -0300

    feat: implementar funcionalidade atualizar funcionário pelo id.

--- conssecionaria-backend/src/main/java/com/conssecionaria/conssecionaria_backend/controller/EmployeeController.java
+++ conssecionaria-backend/src/main/java/com/conssecionaria/conssecionaria_backend/controller/EmployeeController.java
@@ -2,7 +2,9 @@ package com.conssecionaria.conssecionaria_backend.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,8 +26,15 @@ public class EmployeeController {
 
 	@PostMapping
 	public ResponseEntity<EmployeeResponseDTO> registerEmployee(@RequestBody @Valid EmployeeRequestDTO dto) {
-		EmployeeResponseDTO response = this.employeeService.registerEmployee(dto);
+		EmployeeResponseDTO response = employeeService.registerEmployee(dto);
 
 		return ResponseEntity.status(HttpStatus.CREATED).body(response);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<EmployeeResponseDTO> updateEmployeeById(@PathVariable String id, @RequestBody @Valid EmployeeRequestDTO dto) {
+		EmployeeResponseDTO response = employeeService.updateEmployeeById(id, dto);
+		
+		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
 }
