29892f094a119503ce42c558f76fe95166688645
User <User@192.168.1.21>
2026-06-07 22:49:42 -0300

    style: formatação da classe de execução.

--- conssecionaria-backend/src/main/java/com/conssecionaria/conssecionaria_backend/ConssecionariaBackendApplication.java
+++ conssecionaria-backend/src/main/java/com/conssecionaria/conssecionaria_backend/ConssecionariaBackendApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConssecionariaBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ConssecionariaBackendApplication.class, args);
 	}
-
 }
