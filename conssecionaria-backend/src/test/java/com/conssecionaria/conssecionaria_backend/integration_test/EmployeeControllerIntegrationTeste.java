596480e34b0247adb8a28d23c00c3ba19fc462c7
User <User@192.168.1.21>
2026-06-13 17:53:07 -0300

    test: implementar teste de integração de funcionalidade lista funcionários com filtro nome, status do funcionario ou tipo do funcionário.

--- conssecionaria-backend/src/test/java/com/conssecionaria/conssecionaria_backend/integration_test/EmployeeControllerIntegrationTeste.java
+++ conssecionaria-backend/src/test/java/com/conssecionaria/conssecionaria_backend/integration_test/EmployeeControllerIntegrationTeste.java
@@ -1,6 +1,7 @@
 package com.conssecionaria.conssecionaria_backend.integration_test;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -8,7 +9,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.UUID;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -79,5 +79,45 @@ class EmployeeControllerIntegrationTeste {
 
 		mockMvc.perform(put("/employees/update-employee-by-id/" + employees.get(0).getId()).contentType(MediaType.APPLICATION_JSON)
 				.accept(MediaType.APPLICATION_JSON).content(json)).andExpect(status().isOk()).andDo(print());
+	}
+	
+	@Test
+	void shouldFindEmployeeByIdAndReturn200Status() throws Exception {
+		Employee employee1 = new Employee(null, "name1", "1111111111",
+				"email1@gmail.com", "81911111111", LocalDate.now().withYear(1991), "09458274400", new BigDecimal("1500.00"),
+				100, EmployeeStatus.ACTIVE, EmployeeType.SALLER, null);
+		
+		List<Employee> employees = List.of(employee1);
+
+		employeeRepository.saveAll(employees);
+		
+		mockMvc.perform(get("/employees/find-employee-by-id/" + employees.get(0).getId()).contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andDo(print());
+	}
+	
+	@Test
+	void shouldListEmployeesAndReturn200Status() throws Exception {
+		Employee employee1 = new Employee(null, "name1", "1111111111",
+				"email1@gmail.com", "81911111111", LocalDate.now().withYear(1991), "09458274400", new BigDecimal("1500.00"),
+				100, EmployeeStatus.ACTIVE, EmployeeType.SALLER, null);
+		
+		Employee employee2 = new Employee(null, "name2", "2222222222",
+				"email2@gmail.com", "81922222222", LocalDate.now().withYear(1991), "43468998465", new BigDecimal("2500.00"),
+				null, EmployeeStatus.ACTIVE, EmployeeType.MANAGER, null);
+		
+		Employee employee3 = new Employee(null, "name3", "3333333333",
+				"email3@gmail.com", "81933333333", LocalDate.now().withYear(1991), "27406772432", new BigDecimal("2000.00"),
+				null, EmployeeStatus.ACTIVE, EmployeeType.ASSISTANT_MANAGER, null);
+		
+		Employee employee4 = new Employee(null, "name4", "4444444444",
+				"email4@gmail.com", "81944444444", LocalDate.now().withYear(1991), "66134259403", new BigDecimal("1500.00"),
+				100, EmployeeStatus.ACTIVE, EmployeeType.SALLER, null);
+		
+		List<Employee> employees = List.of(employee1, employee2, employee3, employee4);
+
+		employeeRepository.saveAll(employees);
+		
+		mockMvc.perform(get("/employees/list-employees").queryParam("name", "n").queryParam("employeeType", "SALLER").contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andDo(print());
 	}
 }
