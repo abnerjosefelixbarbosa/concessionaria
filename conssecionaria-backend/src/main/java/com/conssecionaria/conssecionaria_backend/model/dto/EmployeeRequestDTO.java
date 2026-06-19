f39572446641780f138cf70aa7a8e61d779a4f07
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2026-06-19 19:46:31 -0300

    docs: implmentar e corrigir documentação do swagger.

--- conssecionaria-backend/src/main/java/com/conssecionaria/conssecionaria_backend/model/dto/EmployeeRequestDTO.java
+++ conssecionaria-backend/src/main/java/com/conssecionaria/conssecionaria_backend/model/dto/EmployeeRequestDTO.java
@@ -21,43 +21,43 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Schema(description = "Dados de um funcionário.")
+@Schema(description = "Dados de requisição do funcionário.")
 public class EmployeeRequestDTO {
-	@Schema(description = "Nome do funcionário.", example = "Nome1", requiredMode = Schema.RequiredMode.REQUIRED)
+	@Schema(description = "Nome do funcionário.", requiredMode = Schema.RequiredMode.REQUIRED)
 	@NotEmpty(message = "Nome deve ser obrigatório.")
 	@NotNull(message = "Nome deve ser obrigatório.")
 	@Size(message = "Nome deve ter até 100 caracteres.", max = 100)
 	private String name;
-	@Schema(description = "Matrícula do funcionário.", example = "1111111111", requiredMode = Schema.RequiredMode.REQUIRED)
+	@Schema(description = "Matrícula do funcionário.", requiredMode = Schema.RequiredMode.REQUIRED)
 	@NotNull(message = "Matrícula deve ser obrigatória.")
 	@Pattern(message = "Matrícula deve ter 10 caracteres numéricos.", regexp = "^\\d{10}$")
 	private String matriculation;
-	@Schema(description = "Email do funcionário.", example = "email1@gmail.com", requiredMode = Schema.RequiredMode.REQUIRED)
+	@Schema(description = "Email do funcionário.", requiredMode = Schema.RequiredMode.REQUIRED)
 	@NotEmpty(message = "Email deve ser valido.")
 	@NotNull(message = "Email deve ser valido.")
 	@Email(message = "Email deve ser valido.")
 	private String email;
-	@Schema(description = "Telefone do funcionário", example = "81911111111", requiredMode = Schema.RequiredMode.REQUIRED)
+	@Schema(description = "Telefone do funcionário", requiredMode = Schema.RequiredMode.REQUIRED)
 	@NotEmpty(message = "Telefone deve ser obrigatório.")
 	@NotNull(message = "Telefone deve ser obrigatório.")
 	@Size(message = "Telefone deve ter até 30 caracteres.", max = 30)
 	private String phone;
-	@Schema(description = "Data de nascimento do funcionário", example = "1991-01-01", requiredMode = Schema.RequiredMode.REQUIRED)
+	@Schema(description = "Data de nascimento do funcionário", requiredMode = Schema.RequiredMode.REQUIRED)
 	@NotNull(message = "Data de nascimento deve ser obrigatória.")
 	private LocalDate birthDate;
-	@Schema(description = "CPF do funcionário", example = "81552834450", requiredMode = Schema.RequiredMode.REQUIRED)
+	@Schema(description = "CPF do funcionário", requiredMode = Schema.RequiredMode.REQUIRED)
 	@NotNull(message = "CPF deve ser valido.")
 	@CPF(message = "CPF deve ser valido.")
 	private String cpf;
-	@Schema(description = "Salário do funcionário", example = "1500.00", requiredMode = Schema.RequiredMode.REQUIRED)
+	@Schema(description = "Salário do funcionário", requiredMode = Schema.RequiredMode.REQUIRED)
 	@NotNull(message = "Salário deve ser obrigatório.")
 	private BigDecimal salary;
-	@Schema(description = "Comissão do funcionário", example = "100", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+	@Schema(description = "Comissão do funcionário", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
 	private Integer commission;
-	@Schema(description = "Status do funcionário", example = "ACTIVE", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+	@Schema(description = "Status do funcionário", requiredMode = Schema.RequiredMode.REQUIRED)
 	@NotNull(message = "Status do funcionário deve ser obrigatório.")
 	private EmployeeStatus employeeStatus;
-	@Schema(description = "Tipo do funcionário", example = "MANAGER", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+	@Schema(description = "Tipo do funcionário", requiredMode = Schema.RequiredMode.REQUIRED)
 	@NotNull(message = "Tipo de funcionário deve ser obrigatório.")
 	private EmployeeType employeeType;
 }
