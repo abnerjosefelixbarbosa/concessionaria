f4b301166fe771777a3c795c8421daeb784630f5
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2026-06-27 16:20:08 -0300

    docs: documentar funcionalidade registrar cliente no swagger.

--- backend/src/main/java/com/concessionaria/backend/controller/CustomerController.java
+++ backend/src/main/java/com/concessionaria/backend/controller/CustomerController.java
@@ -5,12 +5,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.concessionaria.backend.model.dto.CustomerRequestDTO;
 import com.concessionaria.backend.model.dto.CustomerResponseDTO;
 import com.concessionaria.backend.model.service.CustomerService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 
 @RestController
@@ -22,6 +26,10 @@ public class CustomerController {
 		this.customerService = customerService;
 	}
 
+	@ApiResponses(value = { @ApiResponse(responseCode = "201", description = "registra um cliente."),
+			@ApiResponse(responseCode = "400", description = "retorna um erro de requesição.") })
+	@ResponseStatus(value = HttpStatus.CREATED)
+	@Operation(summary = "registra cliente.", description = "registra um cliente.")
 	@PostMapping(value = "/register-customer")
 	public ResponseEntity<CustomerResponseDTO> registerCustomer(@RequestBody @Valid CustomerRequestDTO dto) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(customerService.registerCustomer(dto));
--- backend/src/main/java/com/concessionaria/backend/model/dto/CustomerResponseDTO.java
+++ backend/src/main/java/com/concessionaria/backend/model/dto/CustomerResponseDTO.java
@@ -1,10 +1,18 @@
 package com.concessionaria.backend.model.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Dados de resposta do cliente.")
 public record CustomerResponseDTO(
+		@Schema(description = "Id do cliente.")
 		String id,
+		@Schema(description = "Nome do cliente.")
 		String name,
+		@Schema(description = "Documento do cliente.")
 		String document,
+		@Schema(description = "Email do cliente.")
 		String email,
+		@Schema(description = "Telefone do cliente.")
 		String phone
 ) {
 
